fdf5a5c096340510c2af092aa6ec6e78563fd4be
Amine KASSI <amine.k4595@gmail.com>
2024-10-15 11:15:26 +0200

    Update

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -1,3 +1,5 @@
+// spectralnext/components/ClientLayout.tsx
+
 "use client";
 
 import { ReactNode, useEffect, useState } from "react";
@@ -6,6 +8,8 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Loader from "@/components/Loader";
 import React from "react";
+import { BackTop } from "antd"; // Importation de BackTop
+import { FaArrowUp } from "react-icons/fa"; // Importation de l'icône flèche vers le haut
 
 interface ClientLayoutProps {
   children: ReactNode;
@@ -50,6 +54,54 @@ export default function ClientLayout({
       {shouldShowNavbar && <Navbar />}
       <div>{children}</div>
       {shouldShowFooter && <Footer backgroundImage={footerImage} />}
+
+      {/* Back to Top Button */}
+      <BackTop visibilityHeight={200}>
+        <div className="back-to-top-button">
+          <FaArrowUp className="text-teal-500 text-2xl" />
+        </div>
+      </BackTop>
+
+      {/* Custom Styles */}
+      <style jsx global>{`
+        /* Animation de fade-in */
+        @keyframes fade-in {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+
+        /* Stylisation du bouton Back to Top */
+        .back-to-top-button {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 48px; /* Ajustez la taille selon vos besoins */
+          width: 48px;  /* Ajustez la taille selon vos besoins */
+          background-color: #000; /* Couleur de fond noire */
+          border-radius: 50%;
+          box-shadow: 0 0 10px rgba(0, 255, 255, 0.7),
+                      0 0 20px rgba(0, 255, 255, 0.6),
+                      0 0 30px rgba(0, 255, 255, 0.5);
+          animation: fade-in 1.5s;
+          transition: opacity 0.3s;
+          cursor: pointer;
+        }
+
+        /* Positionnement du bouton Back to Top */
+        .ant-back-top {
+          right: 40px; /* Distance du bord droit */
+          bottom: 40px; /* Distance du bord inférieur */
+        }
+
+        /* Hover effect pour le bouton */
+        .back-to-top-button:hover {
+          opacity: 0.8;
+        }
+      `}</style>
     </>
   );
 }
