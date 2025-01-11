52e6a91c52c1f6b8173dddc2fedcc095ae7c1e30
Prasath.Thamboo <prasath1@hotmail.fr>
2025-01-11 21:01:40 +0100

    final

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -79,7 +79,7 @@ export default function Navbar() {
     // Gestion du scroll pour l'arrière-plan de la navbar
     const handleNavbarScroll = () => {
       if (window.scrollY > 0) {
-        setNavbarBackground("bg-green-400 bg-opacity-75 backdrop-blur-md");
+        setNavbarBackground("bg-transparent bg-opacity-75 backdrop-blur-md");
       } else {
         setNavbarBackground("bg-transparent"); // Remplace bg-gray-700 par bg-transparent
       }
